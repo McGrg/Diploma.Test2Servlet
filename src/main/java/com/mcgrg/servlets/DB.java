8b817bf09e79c5585c586238a691ec7f9b4df18d
McGrg <mcgrg@yandex.ru>
2017-02-15 00:38:24 +0300

    Second commit, MySql doesn't work. Entity class's been  included.

---
+++ src/main/java/com/mcgrg/servlets/DB.java
@@ -0,0 +1,44 @@
+package com.mcgrg.servlets;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by MSI GT70 on 15.02.2017.
+ */
+public class DB {
+    private Connection con = null;
+
+    public DB() throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+        con = sqlquerymysql.sqlDrive();
+    }
+
+    public List<ConstructionSites> getSites() throws SQLException{
+        List<ConstructionSites> list = null;
+        list = new LinkedList<>();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM diploma.master.constructionsite;");
+        while (rs.next()) {
+            ConstructionSites site = new ConstructionSites();
+            site.setConssiteId(rs.getInt("cons_site_id"));
+            site.setConssiteCity(rs.getString("cons_site_city"));
+            site.setConssiteStreet(rs.getString("cons_site_street"));
+            site.setConssiteBilding(rs.getString("cons_site_bilding"));
+            site.setStartDate(rs.getString("start_date"));
+            site.setFinishDate(rs.getString("finish_date"));
+            site.setManager(rs.getString("manager"));
+            list.add(site);
+        }
+        rs.close();
+        stmt.close();
+        return list;
+    }
+
+    public void close()throws SQLException{
+        con.close();
+    }
+}
--- src/main/java/com/mcgrg/servlets/ServletDBMySql.java
+++ src/main/java/com/mcgrg/servlets/ServletDBMySql.java
@@ -1,5 +1,7 @@
 package com.mcgrg.servlets;
 
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 /**
  * Created by MSI GT70 on 14.02.2017.
@@ -18,10 +21,20 @@ public class ServletDBMySql extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("Works.");
-        out.println("</body></html>");
+        response.setCharacterEncoding("UTF-8");
+        try {
+            DB db = new DB();
+            if (db != null) {
+                List<ConstructionSites> sites = db.getSites();
+                String gson= new Gson().toJson(sites);
+                db.close();
+                PrintWriter out = response.getWriter();
+                out.println(gson);
+                out.close();
+            }
+        } catch (Exception e) {
+            String except = e.toString();
+            response.getWriter().write(except);
+        }
     }
 }
