6144d75d3864c2e42afa3d5a6f25fac1bd93dc04
McGrg <mcgrg@yandex.ru>
2017-02-21 09:53:14 +0300

    It's working son of a bitch!!! Included ms&my sql. Gson. Added Users, Usergroups entity. Materials entity, parametr map. InputServlet, executeUpdate,it's successfully sent data to DecktopApp!!!

--- src/main/java/com/mcgrg/servlets/ServletDBMySql.java
+++ src/main/java/com/mcgrg/servlets/ServletDBMySql.java
@@ -20,102 +20,8 @@ import java.util.Map;
 @WebServlet("/ServletDBMySql")
 public class ServletDBMySql extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        response.setCharacterEncoding("UTF-8");
-//        String[] stringSQL=null;
-//        String[] tableName = null;
-//        String report = null;
-//        Map<String, String[]> map = request.getParameterMap();
-//        //Reading the Map
-//        //Works for GET && POST Method
-//        stringSQL = map.get("sql");
-//        tableName = map.get("table");
-//
-//        //--------------------------------------------------------------
-//
-//        try {
-//            DBSearch db = new DBSearch();
-//            switch (tableName[0]) {
-//                case "authentifikation": {
-//                    PrintWriter outsites = response.getWriter();
-//                    outsites.println(" authentifikation table!");
-//                    outsites.close();
-//                }
-//                break;
-//                case "constructionsite": {
-//                    List<ConstructionSites> sites = db.getSites(stringSQL[0]);
-//                    String gsonsites= new Gson().toJson(sites);
-//                    db.close();
-//                    PrintWriter outsites = response.getWriter();
-//                    outsites.println(gsonsites);
-//                    outsites.close();
-//                }
-//                break;
-//                case "debitstandart": {
-//                    PrintWriter outsites = response.getWriter();
-//                    outsites.println(" debitstandart table!");
-//                    outsites.close();
-//                }
-//                break;
-//                case "materials": {
-//                    List<Materials> sites = db.getMaterials(stringSQL[0]);
-//                    String gsonsites= new Gson().toJson(sites);
-//                    db.close();
-//                    PrintWriter outsites = response.getWriter();
-//                    outsites.println(gsonsites);
-//                    outsites.close();
-//                }
-//                break;
-//                case "operations": {
-//                    PrintWriter outsites = response.getWriter();
-//                    outsites.println(" operations table!");
-//                    outsites.close();
-//                }
-//                break;
-//                case "opertype": {
-//                    PrintWriter outsites = response.getWriter();
-//                    outsites.println(" opertype table!");
-//                    outsites.close();
-//                }
-//                break;
-//                case "stock": {
-//                    PrintWriter outsites = response.getWriter();
-//                    outsites.println(" stock table!");
-//                    outsites.close();
-//                }
-//                break;
-//                case "usergroups": {
-//                    PrintWriter outsites = response.getWriter();
-//                    outsites.println(" usergroups table!");
-//                    outsites.close();
-//                }
-//                break;
-//                case "users": {
-//                    PrintWriter outsites = response.getWriter();
-//                    outsites.println(" users table!");
-//                    outsites.close();
-//                }
-//                default: {
-//                    PrintWriter outsites = response.getWriter();
-//                    outsites.println(" users table!");
-//                    outsites.close();
-//                }
-//            }
-//            db.close();
-//        } catch (Exception e) {
-//            report = e.toString();
-//        } finally {
-//            PrintWriter outsites = response.getWriter();
-//            outsites.println(report);
-//            outsites.close();
-        }
-
-        //--------------------------------------------------------------
-
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         response.setCharacterEncoding("UTF-8");
-        String[] stringSQL=null;
+        String[] stringSQL = null;
         String[] tableName = null;
         String report = null;
         Map<String, String[]> map = request.getParameterMap();
@@ -130,81 +36,61 @@ public class ServletDBMySql extends HttpServlet {
             DBSearch db = new DBSearch();
             switch (tableName[0]) {
                 case "authentifikation": {
-                    PrintWriter outsites = response.getWriter();
-                    outsites.println(" authentifikation table!");
-                    outsites.close();
+                    report = " authentifikation table!";
                 }
                 break;
                 case "constructionsite": {
                     List<ConstructionSite> sites = db.getSites(stringSQL[0]);
-                    String gsonsites= new Gson().toJson(sites);
+                    report = new Gson().toJson(sites);
                     db.close();
-                    PrintWriter outsites = response.getWriter();
-                    outsites.println(gsonsites);
-                    outsites.close();
                 }
                 break;
                 case "debitstandart": {
-                    PrintWriter outsites = response.getWriter();
-                    outsites.println(" debitstandart table!");
-                    outsites.close();
+                    report = " debitstandart table!";
                 }
                 break;
                 case "materials": {
                     List<Materials> sites = db.getMaterials(stringSQL[0]);
-                    String gsonsites= new Gson().toJson(sites);
+                    report = new Gson().toJson(sites);
                     db.close();
-                    PrintWriter outsites = response.getWriter();
-                    outsites.println(gsonsites);
-                    outsites.close();
                 }
                 break;
                 case "operations": {
-                    PrintWriter outsites = response.getWriter();
-                    outsites.println(" operations table!");
-                    outsites.close();
+                    report = " operations table!";
                 }
                 break;
                 case "opertype": {
-                    PrintWriter outsites = response.getWriter();
-                    outsites.println(" opertype table!");
-                    outsites.close();
+                    report = " debitstandart table!";
                 }
                 break;
                 case "stock": {
-                    PrintWriter outsites = response.getWriter();
-                    outsites.println(" stock table!");
-                    outsites.close();
+                    report = " stock table!";
                 }
                 break;
                 case "usergroups": {
-                    PrintWriter outsites = response.getWriter();
-                    outsites.println(" usergroups table!");
-                    outsites.close();
+                    report = " usergroups table!";
                 }
                 break;
                 case "users": {
-                    PrintWriter outsites = response.getWriter();
-                    outsites.println(" users table!");
-                    outsites.close();
+                    report = " users table!";
                 }
                 default: {
-                    PrintWriter outsites = response.getWriter();
-                    outsites.println(" users table!");
-                    outsites.println(stringSQL[0] + " "+ tableName[0]);
-                    outsites.close();
+                    report = " There isn't such a table!";
                 }
             }
             db.close();
         } catch (Exception e) {
             report = e.toString();
         } finally {
-//            PrintWriter outsites = response.getWriter();
-//            outsites.println(report);
-//            outsites.close();
+            PrintWriter outsites = response.getWriter();
+            outsites.println(report);
+            outsites.close();
         }
 
         //--------------------------------------------------------------
 
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     }
 }
